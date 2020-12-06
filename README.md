724a61eca37fb3b70ff1259533f3ee323b843d07
TANAPON KRITTINADDAKUL <61050192@kmitl.ac.th>
2020-12-06 18:07:29 +0700

    ui

--- README.md
+++ README.md
@@ -1,35 +1,13 @@
-# Kubyk
-
-### v 0.11
-
-### Python/Flask based template Web UI.
-
-![Alt text](https://github.com/ratibor78/kubyk/blob/master/kubyk1.png?raw=true "Kubyk WEB UI main page")
-![Alt text](https://github.com/ratibor78/kubyk/blob/master/kubyk2.png?raw=true "Kubyk WEB UI main page menu")
-![Alt text](https://github.com/ratibor78/kubyk/blob/master/kubyk3.png?raw=true "Kubyk WEB UI users admin")
-
-Project based on Bootstrap and SQlite, for helping you with creating own WEB UI for some automation tasks. 
-
-From box you'll get a user levels, small Sqlite database for storing any data, some examples of using Python and Flask. Also there is a Dockerfile ready for creating Docker image for quick start. 
-
-## Check what it is: 
-
-```
-$ docker run -d --name kubyk -p 80:80 ratibor78/kubyk
-```
-After open http://yourip:80 in your browser and login = admin, password = admin
-
-
 ## Installation
 
 ### Quick start on your local PC
 
 1) Clone the repository, create environment and install requirements
 ```sh
-$ cd kubyk
+$ cd io_project
 $ virtualenv venv && source venv/bin/activate
 $ pip install -r requirements.txt
-$ python kubyk.py 
+$ python io.py 
 ```
 Open in your browser http://127.0.0.1:5000 login = admin, password = admin
 
--- app/webui.py
+++ app/webui.py
@@ -6,19 +6,16 @@ from flask_wtf import FlaskForm
 from wtforms.validators import (InputRequired, Length, DataRequired)
 from wtforms import (StringField, PasswordField, SelectField)
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask import (render_template, url_for, request, redirect)
+from flask import (render_template, url_for, request, flash, redirect)
 from flask_login import (
     LoginManager, UserMixin, login_user,
     login_required, logout_user, current_user
 )
 
 
-pwd = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+# pwd = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
 
 
 class User(UserMixin, db.Model):
@@ -30,129 +27,38 @@ class User(UserMixin, db.Model):
     def set_password(self, password):
         self.password = generate_password_hash(password, method='sha256')
 
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
-def requires_admin_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        role = current_user.level
-        if not role or role != 'admin':
-            return redirect(url_for('index'))
-        return f(*args, **kwargs)
-    return decorated
+class IndexForm(FlaskForm):
+    tweet_text = StringField('Tweet_Text', validators=[InputRequired(), Length(min=2, max=35)]) # NOQA
+    location = StringField('Location', validators=[InputRequired(), Length(min=1, max=80)]) # NOQA
+    description = StringField('Description', validators=[InputRequired(), Length(min=1, max=80)]) # NOQA
 
 
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=2, max=35)]) # NOQA
-    password = PasswordField('password', validators=[InputRequired(), Length(min=1, max=80)]) # NOQA
 
-
-class CreateForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=2, max=35)]) # NOQA
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=1, max=80)]) # NOQA
-    level = SelectField('Access Level', validators=[DataRequired()], choices=[("", ""), ("admin", "admin"), ("user", "user")]) # NOQA
-
-
-class ChangeForm(FlaskForm):
-    password = PasswordField('password') # NOQA
-    level = SelectField('Access Level', validators=[DataRequired()], choices=[("admin", "admin"), ("user", "user")]) # NOQA
-
-
-@app.route('/users')
-@login_required
-@requires_admin_auth
-def users():
-    users = User.query.all()
-    return render_template('users.html', users=users, user=current_user.username, role=current_user.level) # NOQA
-
-
-@app.route('/userdel/<user>', methods=['GET', 'POST'])
-@login_required
-@requires_admin_auth
-def userdel(user):
-    form = LoginForm()
-    if request.method == 'POST':
-        User.query.filter_by(username=user).delete()
-        db.session.commit()
-        return redirect(url_for('users'))
-    return render_template('delete.html', user=user, form=form) # NOQA
-
-
-@app.route('/')
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = ''
-    form = LoginForm()
-    if form.validate_on_submit():
-        print(form.username.data)
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for('index'))
-        error = 'Invalid Username or Password !'
-        return render_template('login.html', form=form, error=error) # NOQA
-    else:
-        if request.method == 'POST':
-            error = 'Empty or not valid input'
-        return render_template('login.html', form=form, error=error)
-    return render_template('login.html', form=form, error=error)
-
-
-@app.route('/create', methods=['GET', 'POST'])
-@login_required
-@requires_admin_auth
-def create():
-    error = ''
-    form = CreateForm()
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256') # NOQA
-        new_user = User(username=form.username.data, password=hashed_password, level=form.level.data) # NOQA
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('users'))
-    else:
-        if request.method == 'POST':
-            error = 'Empty or not valid input'
-        return render_template('create.html', form=form, error=error)
-    return render_template('create.html', form=form)
-
-
-@app.route('/changeuser/<username>', methods=['GET', 'POST'])
-@login_required
-@requires_admin_auth
-def changeuser(username):
-    error = ''
-    getlevel = User.query.filter_by(username=username).first_or_404()
-    level = getlevel.level
-    form = ChangeForm(level=level)
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=username).first_or_404()
-        if form.password.data != '':
-            user.set_password(form.password.data)
-        user.level = form.level.data
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('users'))
-    else:
-        if request.method == 'POST':
-            error = 'Empty or not valid input'
-        return render_template('changeuser.html', username=username, form=form, error=error) # NOQA
-    return render_template('changeuser.html', username=username, form=form) # NOQA
-
-
-@app.route('/index')
-@login_required
+@app.route('/', methods=['GET', 'POST'])
+# @login_required
 def index():
-    return render_template('index.html', user=current_user.username, role=current_user.level) # NOQA
+    form = IndexForm()
+    tweet_text = form.tweet_text.data
+    location = form.location.data
+    description = form.description.data
+    if form.validate_on_submit():
+
+        flash('Is not IO')
+    
+        print(tweet_text)
+        print(location)
+        print(description)
+
+    return render_template('index.html',form=form) # NOQA
 
 
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
+@app.route('/dashboard', methods=['GET', 'POST'])
+# @login_required
+def dashboard():
+    return render_template('dashboard.html') # NOQA
+
+@app.route('/member', methods=['GET', 'POST'])
+# @login_required
+def member():
+    return render_template('member.html') # NOQA
+
